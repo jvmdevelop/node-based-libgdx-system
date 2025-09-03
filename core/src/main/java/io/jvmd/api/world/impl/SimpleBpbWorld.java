16a952cfe1cfeb5ba4fb14270d21ced116900783
@_jvmd <you@example.com>
2025-09-03 21:25:06 +0500

    @add records

--- core/src/main/java/io/jvmd/api/world/impl/SimpleBpbWorld.java
+++ core/src/main/java/io/jvmd/api/world/impl/SimpleBpbWorld.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -56,6 +57,7 @@ public class SimpleBpbWorld extends PackageNode implements BPBWorld  {
 
     @Override
     public void render() {
+        //todo render only what see a camera
         Box2DDebugRenderer renderer = new Box2DDebugRenderer();
         renderer.render(world, camera.combined);
         actors.values().forEach((actor) -> {
